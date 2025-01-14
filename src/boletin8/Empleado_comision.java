29d628621191949b0a4f5ebf63b02c109537e00d
Jose Manuel Palomo Zambrano <palomo.zambrano.jose.manuel@iescamas.es>
2025-01-14 20:07:27 +0100

    2.commit dos

--- src/boletin8/Empleado_comision.java
+++ src/boletin8/Empleado_comision.java
@@ -13,4 +13,32 @@ class Empleado_comision extends Empleado_base {
 	
 	}
 
+	@Override
+	public double calcularSalario() {
+		// TODO Auto-generated method stub
+		return getSalarioBase()+(porcentaje/100)*ventas;
+	}
+
+	public void setPorcentaje(double porcentaje) {
+		
+		if(porcentaje<0) {
+			System.out.println("el porcentaje no puede ser negativo");
+		}
+		this.porcentaje = porcentaje;
+	}
+
+	public void setVentas(double ventas) {
+		if(ventas<0) {
+			System.out.println("las ventas no puede ser negativo");
+		}
+		this.ventas = ventas;
+	}
+	
+	
+	
+	
+	
+	
+	
+
 }
